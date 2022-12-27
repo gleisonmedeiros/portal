e334d0281edf8717292d92b8520783c7dd1b8698
GleisonPereira <gleisongpm@gmail.com>
2022-12-27 17:17:27 -0300

    1 Metodo Login 2

--- portal/contas/urls.py
+++ portal/contas/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from contas.views import UserLoginView
+
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    #path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
 ]
--- portal/contas/views.py
+++ portal/contas/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+class UserLoginView(LoginView):
+    template_name = 'registration/login.html'
+
+
